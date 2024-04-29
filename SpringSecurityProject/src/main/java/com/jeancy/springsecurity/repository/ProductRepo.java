0f6357d1c297a5baa8345a56e0809786b557fa15
Jeancy Tshibemba Mukendi <jeancyts@gmail.com>
2024-04-29 21:55:30 +0200

    Delete SpringSecurityProject directory

--- SpringSecurityProject/src/main/java/com/jeancy/springsecurity/repository/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.jeancy.springsecurity.repository;
-
-import com.jeancy.springsecurity.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-}
